a038dbd5854433f7761e7e2afaa709f5730b770a
hxsggsz <vt.hugo.2021@gmail.com>
2024-02-08 23:51:35 -0300

    add sqlite to project

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -3,23 +3,6 @@ import "../src/styles/globals.css";
 
 const preview: Preview = {
   parameters: {
-    themes: {
-      clearable: false,
-      list: [
-        {
-          name: "Light",
-          class: [],
-          color: "#ffffff",
-          default: true,
-        },
-        {
-          name: "Dark",
-          // The class dark will be added to the body tag
-          class: ["dark"],
-          color: "#000000",
-        },
-      ],
-    },
     actions: { argTypesRegex: "^on[A-Z].*" },
     controls: {
       matchers: {
